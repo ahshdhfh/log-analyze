3b5921122b10b9b97794863153e489f450080a65
ahshdhfh <51796947+ahshdhfh@users.noreply.github.com>
2023-01-21 21:47:13 +0900

    Update ResultEvt.java
    주석 추가

--- ResultEvt.java
+++ ResultEvt.java
@@ -5,25 +5,22 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import day0104.MainWindow;
-import day0104.SubWindow;
-
 public class ResultEvt extends WindowAdapter implements ActionListener {
 
 	private Result result;
-	
+
 	public ResultEvt(Result result) {
-		this.result=result;
-	}//ResultEvt
-	
+		this.result = result;
+	}// ResultEvt
+
 	@Override
 	public void windowClosing(WindowEvent we) {
 		result.dispose();
-	}//windowClosing
-	
+	}// windowClosing
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-	}//actionPerformed
+
+	}// actionPerformed
 
 }
